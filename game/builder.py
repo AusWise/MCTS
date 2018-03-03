818c29b113bebcd782f13f0057dc2039779c89ae
Adrian Zdobylak <adi0270@gmail.com>
2018-03-03 16:23:59 +0100

    added BoardBuilder

---
+++ game/builder.py
@@ -0,0 +1,25 @@
+from game.board import Board
+from game.card import CardFactory
+from game.hero import Hero
+from copy import deepcopy
+import numpy as np
+from collections import deque
+
+
+class BoardBuilder:
+
+    def generateDeck(self):
+        cardFactory = CardFactory()
+        cards = cardFactory()
+        deck = deepcopy(cards) + deepcopy(cards)
+        deck = np.random.permutation(deck)
+        deck = deque(deck)
+
+        return deck
+
+    def build(self):
+        hero1 = Hero(name="Hero 1", deck=self.generateDeck())
+        hero2 = Hero(name="Hero 2", deck=self.generateDeck())
+        board = Board(hero1, hero2)
+
+        return board
